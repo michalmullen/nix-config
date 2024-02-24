af3656a844290e4f78b46f187f4cef3a836c7490
Mitchell <michalmullen@proton.me>
2024-02-24 16:34:51 +0100

    fix

--- devices/framework.nix
+++ devices/framework.nix
@@ -42,13 +42,13 @@ framework 13 laptop
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  system.stateVersion = "23.04";
+  system.stateVersion = "24.05";
 
   home-manager.users.michalmullen = {
     imports = [
       ../home/home.nix
       ../home/shell.nix
     ];
-    home.stateVersion = "23.04";
+    home.stateVersion = "24.05";
   };
 }
