1bf88ae8c9cd755f54ca9f0b0495f25c022a7fcc
Mitchell <michalmullen@proton.me>
2024-02-24 16:04:35 +0100

    fix

--- nixos/filesystem.nix
+++ nixos/filesystem.nix
@@ -46,55 +46,53 @@ https://github.com/nix-community/impermanence?tab=readme-ov-file
     */
     boot.tmp.useTmpfs = true;
 
-    {
-        fileSystems."/" = {
-            device = "/dev/root_vg/root";
-            fsType = "btrfs";
-            options = [ "subvol=root" ];
-        };
+    fileSystems."/" = {
+        device = "/dev/root_vg/root";
+        fsType = "btrfs";
+        options = [ "subvol=root" ];
+    };
 
-        boot.initrd.postDeviceCommands = lib.mkAfter ''
-            mkdir /btrfs_tmp
-            mount /dev/root_vg/root /btrfs_tmp
-            if [[ -e /btrfs_tmp/root ]]; then
-                mkdir -p /btrfs_tmp/old_roots
-                timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-                mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-            fi
+    boot.initrd.postDeviceCommands = lib.mkAfter ''
+        mkdir /btrfs_tmp
+        mount /dev/root_vg/root /btrfs_tmp
+        if [[ -e /btrfs_tmp/root ]]; then
+            mkdir -p /btrfs_tmp/old_roots
+            timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+            mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+        fi
 
-            delete_subvolume_recursively() {
-                IFS=$'\n'
-                for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-                    delete_subvolume_recursively "/btrfs_tmp/$i"
-                done
-                btrfs subvolume delete "$1"
-            }
-
-            for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-                delete_subvolume_recursively "$i"
+        delete_subvolume_recursively() {
+            IFS=$'\n'
+            for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+                delete_subvolume_recursively "/btrfs_tmp/$i"
             done
+            btrfs subvolume delete "$1"
+        }
 
-            btrfs subvolume create /btrfs_tmp/root
-            umount /btrfs_tmp
-        '';
+        for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+            delete_subvolume_recursively "$i"
+        done
 
-        fileSystems."/persistent" = {
-            device = "/dev/root_vg/root";
-            neededForBoot = true;
-            fsType = "btrfs";
-            options = [ "subvol=persistent" ];
-        };
+        btrfs subvolume create /btrfs_tmp/root
+        umount /btrfs_tmp
+    '';
 
-        fileSystems."/nix" = {
-            device = "/dev/root_vg/root";
-            fsType = "btrfs";
-            options = [ "subvol=nix" ];
-        };
+    fileSystems."/persistent" = {
+        device = "/dev/root_vg/root";
+        neededForBoot = true;
+        fsType = "btrfs";
+        options = [ "subvol=persistent" ];
+    };
 
-        fileSystems."/boot" = {
-            device = "/dev/disk/by-uuid/XXXX-XXXX";
-            fsType = "vfat";
-        };
+    fileSystems."/nix" = {
+        device = "/dev/root_vg/root";
+        fsType = "btrfs";
+        options = [ "subvol=nix" ];
+    };
+
+    fileSystems."/boot" = {
+        device = "/dev/disk/by-uuid/XXXX-XXXX";
+        fsType = "vfat";
     };
 
     # can be created with btrfs mkswapfile
