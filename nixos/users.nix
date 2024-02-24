a53e08054ab2ad6da0571635b8549448e5f8ca22
Mitchell <michalmullen@proton.me>
2024-02-24 15:08:28 +0100

    test

--- nixos/users.nix
+++ nixos/users.nix
@@ -15,6 +15,7 @@ config for users and home-manager
       description = "Mitchell Mullen";
       isNormalUser = true;
       extraGroups = [ "networkmanager" "wheel" ];
+      initalPassword = "testpassword";
 
       # password can be hashed with: nix run nixpkgs#mkpasswd -- -m SHA-512 -s
       # hashedPassword = "$6$IrWnhWvp6BaCsfUD$h24blqdtDkyKzVoI7kDQH5KlkStrsRUAiNCzjGaqL5euHSAu4HYInI7ZwKFGKwm.UF1Ooo62GM0UctOP.CzLo1";
