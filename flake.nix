7e828e732fcb0acab31f3ab98023949212517fcf
Mitchell <michalmullen@proton.me>
2024-02-25 22:27:46 +0100

    test

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
     # packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
     # Formatter for your nix files, available through 'nix fmt'
     # Other options beside 'alejandra' include 'nixpkgs-fmt'
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
 
     # Your custom packages and modifications, exported as overlays
     # overlays = import ./overlays {inherit inputs;};
@@ -68,7 +68,7 @@
         modules = [
           inputs.disko.nixosModules.default
           (import ./disko.nix { device = "/dev/nvme0n1"; })
-          
+
           ./devices/framework.nix
         ];
       };
